f5c911dc33b23efd1136b9bea059b600f3dbbdd0
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-01-29 14:15:39 +0100

    modify app for name preferences

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const appName = 'Minimalistic app';
+const appName = process.env.APP_NAME || 'Node.js App';
 
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
